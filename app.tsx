092fc42caca6c7a85237ac93b5edb82779437ef0
hzluzuoquan <hzluzuoquan@corp.netease.com>
2018-01-30 19:28:52 +0800

    update

--- app.tsx
+++ app.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Provider } from 'mobx-react'
 import App from './containers'
 
 const render = (Component: any) => {
--- store/home.ts
+++ store/home.ts
@@ -1,9 +1,31 @@
-import { observable, action } from "mobx"
+import { observable, action, useStrict } from "mobx"
+import fetch from 'whatwg-fetch'
+
+useStrict(true)
 
 export default class Home {
-  @observable userList
+  @observable userList: Array<any>
 
   constructor() {
     this.userList = []
+  }
+
+  async fetchUsers() {
+    fetch(`http://show.niocn.com/lofter/api/user`, {
+      credentials: 'include'
+    })
+      .then(response => {
+        if (response.status >= 200 && response.status < 300) {
+          return response.json()
+        }
+      })
+      .then(json => {
+        this.setUser(json)
+      })
+  }
+
+  @action
+  setUser(userArray) {
+    this.userList = userArray
   }
 }
